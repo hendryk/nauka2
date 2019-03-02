2ee2ffaffc0f4f6b5bf0a72d4b38fdb89e8ea0e8
krzyzakov <krzyzakov@gmail.com>
2019-03-02 15:59:35 +0100

    trzeci

--- src/main/scala/lsp/Moja.scala
+++ src/main/scala/lsp/Moja.scala
@@ -1,7 +1,7 @@
 package lsp
 
 object Moja extends App {
-    println("Posadzili Bace na skraju")
+    println("Posadzili Bace na skraju Dunajca")
 
     val double: Int => Int = _ * 2
     (1 to 10) foreach double.andThen(println)
